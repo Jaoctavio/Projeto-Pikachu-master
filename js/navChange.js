1f6bc9d98345531f0d604436221b0b872eca7bc7
Jaoctavio <joctbolota@gmail.com>
2022-09-24 17:05:55 -0300

    Dark Mode and Scroll Back To Top Button

---
+++ js/navChange.js
@@ -0,0 +1,31 @@
+function changeBody(page) {
+  fetch(page)
+    .then((res) => res.text())
+    .then((res) => {
+      document.body.innerHTML = res;
+    });
+}
+
+function dark_mode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
---
+++ js/script.js
@@ -0,0 +1,74 @@
+const fetchPokemon = async (pokemon) => {
+  return await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then(async (data) => {
+      return await data.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+fetch("../extern.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.body.innerHTML = data;
+  })
+  .then(() => {
+    function Darkness() {
+      var element = document.body;
+      element.classList.toggle("dark-mode");
+    }
+
+    const pokemonName = document.querySelector(".pokemon__name");
+    const pokemonNumber = document.querySelector(".pokemon__number");
+    const pokemonImage = document.querySelector(".pokemon__image");
+
+    const form = document.querySelector(".form");
+    const input = document.querySelector(".input__search");
+    const buttonPrev = document.querySelector(".btn-prev");
+    const buttonNext = document.querySelector(".btn-next");
+
+    let searchPokemon = 1;
+
+    const renderPokemon = async (pokemon) => {
+      pokemonName.innerHTML = "Loading...";
+      pokemonNumber.innerHTML = "";
+
+      const data = await fetchPokemon(pokemon);
+
+      if (data) {
+        pokemonImage.style.display = "block";
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src =
+          data["sprites"]["versions"]["generation-v"]["black-white"][
+            "animated"
+          ]["front_default"];
+        input.value = "";
+        searchPokemon = data.id;
+      } else {
+        pokemonImage.style.display = "none";
+        pokemonName.innerHTML = "Not found :c";
+        pokemonNumber.innerHTML = "";
+      }
+    };
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      renderPokemon(input.value.toLowerCase());
+    });
+
+    buttonPrev.addEventListener("click", () => {
+      if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+      }
+    });
+
+    buttonNext.addEventListener("click", () => {
+      searchPokemon += 1;
+      renderPokemon(searchPokemon);
+    });
+
+    renderPokemon(searchPokemon);
+  });
